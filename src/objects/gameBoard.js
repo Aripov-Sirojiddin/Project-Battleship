ba19c733e0157c6e79f22231f8ea3d764217c18e
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-06-13 09:53:48 +0500

    Fixed the board object not saving correctly. Added and tested the attack function.

--- src/objects/gameBoard.js
+++ src/objects/gameBoard.js
@@ -2,16 +2,27 @@ const gameBoard = () => {
   const board = {};
 
   const placeShip = (ship, coordinates) => {
-    for (coordinate in coordinates) {
+    coordinates.forEach((coordinate) => {
       board[coordinate] = ship;
-    }
+    });
   };
 
   const checkCoords = (coordinate) => board[coordinate];
 
+  const attack = (coordinate) => {
+    const slotAtCoords = checkCoords(coordinate);
+    if (slotAtCoords != undefined) {
+      slotAtCoords.hit();
+      return "hit!";
+    } else {
+      return "miss...";
+    }
+  };
+
   return {
     placeShip,
     checkCoords,
+    attack,
   };
 };
 
--- src/objects/tests/gameBoard.test.js
+++ src/objects/tests/gameBoard.test.js
@@ -1,6 +1,17 @@
 const gameBoard = require("../gameBoard");
 const ship = require("../ship");
 
+test('Should place "Single square ship" to correct coordinates.', () => {
+  const board = gameBoard();
+
+  const longShip = ship(1);
+  const coords = ["A1"];
+
+  board.placeShip(longShip, coords);
+
+  expect(board.checkCoords("A1")).toBe(longShip);
+});
+
 test('Should place "Long ship" to correct coordinates.', () => {
   const board = gameBoard();
 
@@ -9,7 +20,34 @@ test('Should place "Long ship" to correct coordinates.', () => {
 
   board.placeShip(longShip, coords);
 
-  for (let coordinate in coords) {
-    expect(board.checkCoords(coordinate)).toBe(longShip);
-  }
+  coords.forEach((coord) => {
+    expect(board.checkCoords(coord)).toBe(longShip);
+  });
+});
+
+test("Check empty coords should not hit a ship but recive undefined.", () => {
+  const board = gameBoard();
+
+  const longShip = ship(5);
+  const coords = ["A1", "A2", "A3", "A4", "A5"];
+
+  board.placeShip(longShip, coords);
+
+  expect(board.checkCoords("B1")).toBe(undefined);
+});
+
+test("Check empty coords should not hit a ship but recive undefined.", () => {
+  const board = gameBoard();
+
+  //Place ship
+  const longShip = ship(5);
+  const coords = ["A1", "A2", "A3", "A4", "A5"];
+
+  board.placeShip(longShip, coords);
+
+  //Attack ship
+  expect(board.attack("A1")).toBe("hit!");
+
+  //Check ship health
+  expect(longShip.getHealth()).toBe(4);
 });
